83df4bcab54f1e14d83cd7a67018d949ceeca7fd
iluzdd <75831987+iluzdd@users.noreply.github.com>
2022-02-28 13:10:53 +0200

    Update Assignment1_311447668.py

--- Assignment1_311447668.py
+++ Assignment1_311447668.py
@@ -24,23 +24,9 @@ def my_func(x1,x2,x3):
 # B. 
 
 def convert(hours, minutes=0):
-    s = 60
+    s = 60 
     if hours < 0 or minutes < 0:
         return "Input error!"
-    if isinstance(hours,float) and isinstance(minutes,float):
-        frac, whole = math.modf(hours)
-        fracm, wholem = math.modf(minutes) 
-        frac = s*(frac)
-        fracm = s*(fracm)
-        seconds = (frac+wholem)*s + whole*s*s + fracm 
-    elif isinstance(hours,float) and not isinstance(minutes,float):
-        frac, whole = math.modf(hours)
-        frac = s*(frac)  
-        seconds = (frac+minutes)*s + whole*s*s
-    elif not isinstance(hours,float) and isinstance(minutes,float):
-        fracm, wholem = math.modf(minutes)   
-        fracm = s*(fracm)
-        seconds = (wholem)*s + hours*s*s + fracm
-    else:
+    else: 
         seconds = hours*s*s + minutes*s
     return seconds
