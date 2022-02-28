4f7f262e69a28f0c11d1c333801ce00cad26823c
iluzdd <75831987+iluzdd@users.noreply.github.com>
2022-02-28 18:13:26 +0200

    Update Assignment1_311447668.py

--- Assignment1_311447668.py
+++ Assignment1_311447668.py
@@ -24,8 +24,8 @@ def my_func(x1,x2,x3):
 # B. 
 
 def convert(hours, minutes=None):
-    s = 60
-    if isinstance(hours,int) and minutes != None:
+    s = 60 
+    if isinstance(hours,int) and isinstance(minutes,int):
         if hours < 0 or minutes < 0:
             return "Input error!"
         else: 
